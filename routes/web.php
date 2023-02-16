bfbffa46386b731c64c487cec65452b08ca2b907
sadghost <sadghost993@gmail.com>
2023-02-16 21:22:04 +1000

    made page rules

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/rules', function (){
+   return view('rules');
+});
+
+Route::get('/about', function (){
+   return view('about');
+});
